e8b8922d6ef4064bd7723228bbaa555ec25dde5d
geethanjali128 <karrageethanjali2003@gmail.com>
2024-04-30 11:35:29 +0530

    env environment

--- food-app/src/utils/firebase.js
+++ food-app/src/utils/firebase.js
@@ -7,7 +7,7 @@ import { getAuth,GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDkCPO9kaAQwtxKIxstZ0-BQohnxdTSKWM",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "food-app-d6da7.firebaseapp.com",
   projectId: "food-app-d6da7",
   storageBucket: "food-app-d6da7.appspot.com",
